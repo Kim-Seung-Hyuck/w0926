be63e983d74f609631947cf741bd5bc5a6930b70
Kim-Seung-Hyuck <67545615+Kim-Seung-Hyuck@users.noreply.github.com>
2022-09-27 12:00:21 +0900

    DB 접속 테스트

---
+++ src/test/java/org/zerock/dao/ConnectTests.java
@@ -0,0 +1,71 @@
+package org.zerock.dao;
+
+import lombok.Cleanup;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ConnectTests {
+
+    @Test
+    public void testConnects100() throws Exception {
+
+        //드라이버 로딩
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        long strat = System.currentTimeMillis();
+
+        for (int i = 0; i < 100; i++) {
+
+            //커넥션
+            @Cleanup Connection connection = dao.ConnectionUtil.INSTANCE.getConnection();
+
+
+            String query = "select now()";
+            //쿼리
+            @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+            resultSet.next(); // 메타 데이터 건너뛰기
+
+            String str = resultSet.getString(1);
+
+            System.out.println(str);
+
+            //close()
+        }
+
+
+        long end = System.currentTimeMillis();
+
+        long gap = end - strat;
+
+        System.out.println("Time : " + gap);
+
+
+    }
+
+    public void testConnectsDS() throws Exception {
+
+        Connection connection = dao.ConnectionUtil.INSTANCE.getConnection();
+
+        String query = "select now()";
+        //쿼리
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next(); // 메타 데이터 건너뛰기
+
+        String str = resultSet.getString(1);
+
+        System.out.println(str);
+
+        //close();
+    }
+
+}
